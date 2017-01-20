e9e48f74b2a6ac033d2c061f8628dbb464b642e7
kallol-brc <kallol.brc@gmail.com>
2017-01-20 12:32:59 +0530

    Update SoundSensorOperatedACBulb.ino

--- Day-7/SoundSensorOperatedACBulb/SoundSensorOperatedACBulb.ino
+++ Day-7/SoundSensorOperatedACBulb/SoundSensorOperatedACBulb.ino
@@ -1,26 +1,58 @@
-int relayPin = 7;
-int soundPin = 2;
-int soundSensorValue = 0;
+//************************************************************/
+// Step-1: CONFIGURE VARIABLES
+//************************************************************/
+int _pinD10 = 10; // pin 10 to receive input from sensor
+int _pinD7 = 7; // pin 7 to send signal to relay
+int _soundLevel = HIGH; // variable for storing output of sensor
+boolean _bRelayState = false; // start by assuming OFF state
+unsigned long _lastSoundEventTime; // variable to store the time  
+                                   // of the last sound event
+int _gapBetween2Sounds = 1000; // gap between 2 sound events
 
-void setup()
+//************************************************************/
+// Step-2: INITIALIZE I/O PARAMETERS
+//************************************************************/
+void setup ()
 {
-  pinMode(relayPin, OUTPUT);
-  pinMode(soundPin, INPUT);       
+  Serial.begin(9600);
+  // configure the pins to be used for I/O
+  pinMode (_pinD10, INPUT) ; // input from the sound sensor
+  pinMode(_pinD7, OUTPUT); // output to relay
 }
 
-void loop()
+//************************************************************/
+// Step-3: MAIN PROGRAM
+//************************************************************/
+void loop ()
 {
-  soundSensorValue = analogRead(soundPin);
-
-  if(soundSensorValue < 509)
+  _soundLevel = digitalRead (_pinD10) ; // read sound level
+  if (_soundLevel == LOW) // if a sound occurs
   {
-    //Switch ON the Relay
-    digitalWrite(relayPin, 0);
-    
-    //Wait for 10 seconds
-    delay(10000);
-  
-    //Switch OFF the Relay
-    digitalWrite(relayPin, 1);
+    if (_bRelayState == false) // if relay is in OPEN state
+    {
+      //if event is after at least 1 second of the previous event
+      if( (millis()-_lastSoundEventTime) > _gapBetween2Sounds)
+      {
+        _lastSoundEventTime = millis(); // record event time
+        _bRelayState = true;
+        Serial.println("Switching ON the relay");
+        
+        //Switch ON the Relay
+        digitalWrite(_pinD7, 0);
+      }
+    }
+    if (_bRelayState == true) //if relay is already ON
+    {
+      //if event is after at least 1 second of the previous event
+      if( (millis()-_lastSoundEventTime) > _gapBetween2Sounds)
+      {
+        _lastSoundEventTime = millis(); // record event time          
+        _bRelayState = false;
+        Serial.println("Switching OFF the relay");
+
+        //Switch OFF the Relay
+        digitalWrite(_pinD7, 1);
+      }
+    }
   }
 }
